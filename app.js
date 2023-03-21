edbe7e6d306da8756764f29d9cdb2dd6086bed4e
maraarrieche <arrieche.mara@gmail.com>
2023-03-21 19:36:08 +0000

    domain generator Mariana

---
+++ app.js
@@ -0,0 +1,16 @@
+let pronoun = ['the','our'];
+let adj = ['great', 'big' ];
+let noun = ['jogger','racoon'];
+let ext = ['.com','.net','.es','.io','.us','.org']
+
+ for(let i = 0; i < pronoun.length; i++){
+     for(let j = 0; j < adj.length; j++){
+         for(let k = 0; k < noun.length; k++){
+            for(let m = 0; m < ext.length; m++){
+         console.log(pronoun[i] + adj[j] + noun[k] + ext[m])
+     }
+ }
+     }
+ }
+
+
